d7cea19996980e3094a909994edb4094eaba8628
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-05-20 22:51:21 -0300

    select sort validado

--- SelectSort/selectsort.js
+++ SelectSort/selectsort.js
@@ -1,34 +1,58 @@
 let lista = [15, 9, 2, 20, 5];
-console.log(lista);
-function retornaMenor(lista, posicao, posicaoAtual) {
-  let length = lista.length - 1;
-  console.log(
-    "atual",
-    lista[posicaoAtual],
-    "compara",
-    lista[posicao],
-    "posicao",
-    posicao
-  );
-  if (lista[posicaoAtual] > lista[posicao]) {
-    if (posicao < length) {
-      return retornaMenor(lista, posicao + 1, posicao);
+
+function formaRecursiva(lista) {
+  console.log(lista);
+  function retornaMenor(lista, posicaoAtual) {
+    console.log(
+      "atual",
+      lista[posicaoAtual],
+      "compara",
+      lista[posicao],
+      "posicao",
+      posicao
+    );
+
+    if (lista[posicaoAtual] > lista[posicao]) {
+      if (posicao < length) {
+        return retornaMenor(lista, posicao + 1, posicao);
+      } else {
+        return posicao;
+      }
+    } else if (posicao < length) {
+      return retornaMenor(lista, posicao + 1, posicaoAtual);
     } else {
-      console.log("retorna", lista[posicao]);
-      return posicao;
+      return posicaoAtual;
     }
-  } else if (posicao < length) {
-    return retornaMenor(lista, posicao + 1, posicaoAtual);
-  } else {
-    console.log("retorna", lista[posicaoAtual]);
-    return posicaoAtual;
   }
+
+  for (let i = 0; i < 4; i++) {
+    const x = lista[i];
+    let menor = retornaMenor(lista, i);
+    lista[i] = lista[menor];
+    lista[menor] = x;
+  }
+
+  console.log(lista);
 }
 
-for (let i = 0; i < lista.length - 1; i++) {
-  const x = lista[i];
-  let menor = retornaMenor(lista, i + 1, i);
-  lista[i] = lista[menor];
-  lista[menor] = x;
+function forWayTodo(lista, tamanho) {
+  console.log(lista);
+
+  for (let i = 0; i < tamanho; i++) {
+    menorIndice = i;
+
+    for (let j = i + 1; j < tamanho + 1; j++) {
+      if (lista[menorIndice] > lista[j]) {
+        menorIndice = j;
+      }
+    }
+
+    let aux = lista[i];
+    lista[i] = lista[menorIndice];
+    lista[menorIndice] = aux;
+  }
+  return;
 }
+
+forWayTodo(lista, 4);
 console.log(lista);
