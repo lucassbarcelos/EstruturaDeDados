e1c458a204e300ec373da3ef20a42c00e4fbfe37
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-06-17 00:22:03 -0300

    shell sort finalizado

---
+++ shellSort/shellSort.js
@@ -0,0 +1,20 @@
+let teste = [5, 4, 9, 7, 3, 0, 1, 2];
+console.log("before", teste);
+function shellSort(array, tamanho) {
+  let outro = parseInt(tamanho / 2);
+  while (outro > 0) {
+    for (let i = 0; i < tamanho - 1; i++) {
+      trocaOuNaoTroca(array, i, outro, tamanho - 1);
+    }
+    outro = parseInt(outro / 2);
+  }
+}
+function trocaOuNaoTroca(array, index, divisor, tamanho) {
+  if (array[index] > array[index + divisor] && index + divisor < tamanho) {
+    let aux = array[index];
+    array[index] = array[index + divisor];
+    array[index + divisor] = aux;
+  }
+}
+shellSort(teste, 8);
+console.log("after", teste);
